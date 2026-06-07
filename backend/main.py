28387375f9ac8f93eaa4061b5756d4b73fb685de
Prasad-Kedar <prasadkedar04@gmail.com>
2026-06-07 13:10:46 +0530

    Implemented alert ingestion system

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,53 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import json
+import os
 
 app = FastAPI()
 
+ALERT_FILE = "data/alerts.json"
+
+
+class Alert(BaseModel):
+    source: str
+    severity: str
+    event: str
+    ip: str
+
+
 @app.get("/")
-def root():
-    return {"message": "CyberGuard AI Backend Running"}
+def home():
+    return {"message": "CyberGuard AI Running"}
+
+
+@app.post("/alert")
+def create_alert(alert: Alert):
+
+    os.makedirs("data", exist_ok=True)
+
+    if not os.path.exists(ALERT_FILE):
+        with open(ALERT_FILE, "w") as f:
+            json.dump([], f)
+
+    try:
+        with open(ALERT_FILE, "r") as f:
+            alerts = json.load(f)
+    except:
+        alerts = []
+
+    alerts.append(alert.model_dump())
+
+    with open(ALERT_FILE, "w") as f:
+        json.dump(alerts, f, indent=4)
+
+    return {"status": "success"}
+
+
+@app.get("/alerts")
+def get_alerts():
+
+    if not os.path.exists(ALERT_FILE):
+        return []
+
+    with open(ALERT_FILE, "r") as f:
+        return json.load(f)
--- frontend/dashboard.py
+++ frontend/dashboard.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import requests
+import pandas as pd
 
 st.set_page_config(
     page_title="CyberGuard AI",
@@ -6,7 +8,54 @@ st.set_page_config(
 )
 
 st.title("CyberGuard AI")
+st.subheader("SOC Alert Dashboard")
 
-st.subheader("Autonomous SOC Agent")
+st.markdown("---")
 
-st.success("System Online")
+source = st.selectbox(
+    "Source",
+    ["Firewall", "EDR", "SIEM", "IDS"]
+)
+
+severity = st.selectbox(
+    "Severity",
+    ["Low", "Medium", "High", "Critical"]
+)
+
+event = st.text_input("Event")
+
+ip = st.text_input("IP Address")
+
+if st.button("Submit Alert"):
+
+    payload = {
+        "source": source,
+        "severity": severity,
+        "event": event,
+        "ip": ip
+    }
+
+    response = requests.post(
+        "http://127.0.0.1:8000/alert",
+        json=payload
+    )
+
+    if response.status_code == 200:
+        st.success("Alert Submitted")
+
+
+        st.markdown("---")
+
+st.header("Recent Alerts")
+
+response = requests.get(
+    "http://127.0.0.1:8000/alerts"
+)
+
+if response.status_code == 200:
+
+    alerts = response.json()
+
+    if len(alerts) > 0:
+        df = pd.DataFrame(alerts)
+        st.dataframe(df)
